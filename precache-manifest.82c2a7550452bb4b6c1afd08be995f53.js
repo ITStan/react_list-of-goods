2b1150524ebcbfa6a2e59905ad8046b7152981e6
ITStan <s.stanislavskaya@gmail.com>
2019-11-04 01:22:11 +0200

    Updates

---
+++ precache-manifest.82c2a7550452bb4b6c1afd08be995f53.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a115d43c42088fd7e1da294cc2bb49c8",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "d4fc781615581d7f91f5",
+    "url": "/react_list-of-goods/static/js/2.faa61acf.chunk.js"
+  },
+  {
+    "revision": "7f831233cc343ca1c3b9",
+    "url": "/react_list-of-goods/static/js/main.035fd9df.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.82c2a7550452bb4b6c1afd08be995f53.js"
 );
 
 self.addEventListener('message', (event) => {
